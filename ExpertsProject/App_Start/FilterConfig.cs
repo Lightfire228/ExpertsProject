dee22f36d58a5b8eea803a48899f2e0367d55383
lightfire228 <lightfire228@gmail.com>
2016-11-07 15:27:23 -0500

    Initial commit

---
+++ ExpertsProject/App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ExpertsProject {
+	public class FilterConfig {
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+			filters.Add(new HandleErrorAttribute());
+		}
+	}
+}
---
+++ ExpertsProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExpertsProject.MvcApplication" Language="C#" %>
