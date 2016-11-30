b87ee1a7f0142c0c75226a52ff854a8ce88c5478
lightfire228 <lightfire228@gmail.com>
2016-11-30 00:08:20 -0500

    push darnit

---
+++ ExpertsProject/Models/UserViewModels/SearchViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExpertsProject.Models.UserViewModels {
+	public class SearchViewModel {
+
+		public string SearchText {
+			get; set;
+		}
+
+		public IEnumerable<Expert> Experts {
+			get; set;
+		}
+	}
+}
