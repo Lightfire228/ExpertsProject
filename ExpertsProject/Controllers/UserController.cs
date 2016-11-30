f5a0a4b4fbf7faf79f27bc1d0224705edbe80621
lightfire228 <lightfire228@gmail.com>
2016-11-29 21:58:36 -0500

    merging andrew

--- ExpertsProject/Controllers/UserController.cs
+++ ExpertsProject/Controllers/UserController.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using ExpertsProject.Models.UserViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
-namespace ExpertsProject.Controllers {
-	public class UserController {
+namespace ExpertsProject.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		public ActionResult Search(SearchViewModel model) {
+
+
+			return View(model);
+		}
 
 	}
 }
