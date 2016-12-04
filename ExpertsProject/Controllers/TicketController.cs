34fa8113aa66b78924f583de21c6211336ec60f4
lightfire228 <lightfire228@gmail.com>
2016-12-04 01:18:42 -0500

    Added ticket creation, added title field to ticket DB

--- ExpertsProject/Controllers/TicketController.cs
+++ ExpertsProject/Controllers/TicketController.cs
@@ -1,4 +1,5 @@
 ﻿using ExpertsProject.Models;
+using ExpertsProject.Models.TicketViewModels;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
@@ -26,8 +27,61 @@ namespace ExpertsProject.Controllers
         }
 
 		public ActionResult UserIndex() {
-			
+
+			if (!isLoggedIn()) {
+				return RedirectToAction("Login", "Account");
+			}
+
+			IEnumerable<Ticket> modelTickets;
+			IEnumerable<Ticket> tickets = _dbContext.Ticket.ToList();
+
+			ApplicationUser user = _dbContext.Users.Find(System.Web.HttpContext.Current.User.Identity.GetUserId());
+
+			modelTickets = from ticket in tickets
+						   where ticket.CreatedBy == user.Id
+						   select ticket;
+
+			TicketsList model = new TicketsList();
+			model.Tickets = modelTickets;
+			model.IsMe = true;
+
+			return View(model);
+		}
+
+		[HttpGet]
+		public ActionResult New() {
+		
+			if (!isLoggedIn())
+				return RedirectToAction("Login", "Account");
+
 			return View();
+		}
+
+		[HttpPost]
+		public ActionResult New(NewTicketViewModel model) {
+
+			if (!isLoggedIn())
+				return RedirectToAction("Oops");
+
+			Ticket ticket        = new Ticket();
+			Message message      = new Message();
+			ApplicationUser user = _dbContext.Users.Find(System.Web.HttpContext.Current.User.Identity.GetUserId());
+
+			ticket.Title = model.Title;
+			ticket.Status = TicketStatus.OPEN;
+			ticket.User = user;
+			ticket.Created = System.DateTime.Now;
+
+			message.BodyText = model.BodyText;
+			message.Date = ticket.Created;
+			message.Ticket = ticket;
+			message.User = user;
+
+			_dbContext.Ticket.Add(ticket);
+			_dbContext.Messages.Add(message);
+			_dbContext.SaveChanges();
+
+			return RedirectToAction("UserIndex");
 		}
 
 		public ActionResult ExpertIndex() {
