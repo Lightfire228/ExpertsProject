3438137b7a06648c189b68e8f9e3644041f4af6e
ShuChing <Ching1@marshall.edu>
2016-12-04 14:51:16 -0500

    admin updates

--- ExpertsProject/Controllers/AdminController.cs
+++ ExpertsProject/Controllers/AdminController.cs
@@ -3,87 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ExpertsProject.Models;
 
 namespace ExpertsProject.Controllers
 {
     public class AdminController : Controller
     {
+        private ApplicationDbContext _dbContext;
+
+        public AdminController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
         // GET: Admin
         public ActionResult Index()
         {
             return View();
         }
-
-        // GET: Admin/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Verify(String id)
         {
-            return View();
-        }
+            var expert = _dbContext.Experts.SingleOrDefault(v => v.Id == id);
 
-        // GET: Admin/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+            if (expert == null)
+                return HttpNotFound();
 
-        // POST: Admin/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Admin/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Admin/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Admin/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Admin/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+            return View(expert);
         }
     }
 }
